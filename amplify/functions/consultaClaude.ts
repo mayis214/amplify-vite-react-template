c899cd1af8394efa7fee754ca5b90f0026236f49
mayis214 <mayis214@gmail.com>
2024-05-31 21:22:09 -0500

    SinFuncionBedrock

--- amplify/functions/consultaClaude.ts
+++ amplify/functions/consultaClaude.ts
@@ -1,17 +1,17 @@
 
-import { Handler, ConnectContactFlowEvent, LexEvent } from 'aws-lambda';
+/*import { Handler, ConnectContactFlowEvent, LexEvent } from 'aws-lambda';
 import { BedrockRuntimeClient, InvokeModelCommand, InvokeModelCommandInput } from "@aws-sdk/client-bedrock-runtime";
 const client = new BedrockRuntimeClient({region: "us-east-1"});
 
 const lambdaHandler = async (event: LexEvent, context: any) => {
     console.log(event);
     // Crea un cliente de Bedrock Runtime
-    const modelId = "anthropic.claude-3-sonnet-20240229-v1:0";
+    const MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0";
     const input: InvokeModelCommandInput = {
-        modelId,
+        MODEL_ID,
         contentType: "application/json", 
         accept: "application/json", -
-        body:JSON.stringify({ 
+        body:JSON.stringify({,
             "anthropic_version": "bedrock-2023-05-31",
             "max_tokens": 8000,
             "temperature": 0.5,
@@ -32,6 +32,7 @@ const lambdaHandler = async (event: LexEvent, context: any) => {
     statusCode: 200,
     body: responseText
   };
-}
+
 
 export { lambdaHandler };
+*/
