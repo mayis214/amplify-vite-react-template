2cd42ed1826879e9112bb0384b27308de96f11c7
mayis214 <mayis214@gmail.com>
2024-05-31 22:42:16 -0500

    DefinicionBackendCrearDynamoYBucket

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -3,3 +3,13 @@ export const consultaClaude = defineFunction({
   name: "consultaClaude",
   entry: "./consultaClaude.ts"
 });
+
+export const crearBucket = defineFunction({
+  name: "crearBucket",
+  entry: "./crearBucket.ts"
+});
+
+export const crearDynamo = defineFunction({
+  name: "crearDynamo",
+  entry: "./crearDynamo.ts"
+});
