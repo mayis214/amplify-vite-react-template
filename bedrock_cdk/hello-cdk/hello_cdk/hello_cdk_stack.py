34d053c680fc1949447d6b88a13b7c3a62b0daf2
mayis214 <mayis214@gmail.com>
2024-06-05 19:00:13 -0500

    Inclusion front javascrip como ejemplo

--- bedrock_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
+++ bedrock_cdk/hello-cdk/hello_cdk/hello_cdk_stack.py
@@ -1,6 +1,7 @@
 from aws_cdk import (
     Stack,
     aws_s3 as s3,
+
 )
 from constructs import Construct
 
@@ -9,4 +10,4 @@ class HelloCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True)
+     
